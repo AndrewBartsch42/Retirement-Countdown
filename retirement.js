eccb18955329a3abcc4f295dadaa3369783d496c
Kaleb Aregay <156873063+Kalebk24@users.noreply.github.com>
2026-03-26 14:17:52 -0500

    Add files via upload

--- retirement.js
+++ retirement.js
@@ -31,52 +31,86 @@ const processEntries = (evt) => {
     evt.preventDefault();
     resetForm()
 
+    const invest =  parseFloat(investIn.value);
+    const add = parseFloat(addIn.value);
+    const rate = parseFloat(rateIn.value);
 
-    // TODO: Validate Name 
-    // if name == null throw an error else do nothing
-    try {
-        if (nameIn.value == ''){
-            throw new Error("blankName");
-        }
-    } catch (error) {
-        console.log("name field is blank");
-        let name_error_span = document.getElementById("name_error");
-        name_error_span.innerHTML = "Please enter a name field";
+    // Validate the name
+    if (nameIn.value.trim() === "") {
+        $("#name_error").textContent = nameIn.title; // Pulls from title attribute
+        isValid = false;
     }
-    // OLD: error logic for name validation
-    //if (nameIn.textContent == ''){
-    //   throw console.error("name field is null");
-    //}
-    
 
-    // Validate Email
-    // check formatting == xxxx@xxx.xxx with a regex expression
-    try {
-        let validEmailFormat = /^[a-zA-Z0-9_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/; // regex expression for a valid email address
-        if (!validEmailFormat.test(emailIn.value) || emailIn.value == ''){
-            throw new Error("invalidEmail")
+        const emailPattern = /^[\w\.\-]+@[\w\.\-]+\.[a-zA-Z]+$/;
+        if (!emailPattern.test(emailIn.value.trim())) {
+            $("#email_error").textContent = emailIn.title;
+            isValid = false;
         }
-    } catch (error) {
-        console.log("invalid email")
-        let email_error_span = document.getElementById("email_error");
-        email_error_span.innerHTML = "Please enter a valid email address";
-    }
-    
-    // TODO: Validate Date
-    // make sure the date is at least in the present and not the past
 
-    // TODO: Numeric Validations
-    // check for negative numbers and numeric input
+
+    /* TODO: Validate Date
+        if date is empty
+            display error similar to name logic
+        else
+            years = user's year - the current year
+            if years is less or equal to 0 || greater than 75
+                display error similar to name logic
+
+     */
+    if (dateIn.value.trim() === "") {
+        $("#retire_date_error").textContent = dateIn.title;
+        isValid = false;
+    } else{
+        const today = new Date();
+        const retireDate = new Date(dateIn.value);
+        years = retireDate.getFullYear() - today.getFullYear();
+
+        if (years <= 0 || years > 75){
+            $("#retire_date_error").textContent = dateIn.title;
+            isValid = false;
+        }
+    }
+
+    if (isNaN(invest) || invest < 0) {
+        $("#investment_error").textContent = investIn.title;
+        isValid = false;
+    }
+
+    /*
+     TODO: do the same for the other two numeric input values
+        based on the input field's title data validation message
+     */
+    if (isNaN(add) || add < 0) {
+        $("#add_error").textContent = addIn.title;
+        isValid = false;
+    }
+
+    if (isNaN(rate) || rate < 0) {
+        $("#rate_error").textContent = rateIn.title;
+        isValid = false;
+    }
 
     /* TODO: Code try-catch logic
         try
             if not valid then throw error "Please correct the entries highlighted below."
+            NOTE: otherwise the following 2 statements will run
             document.body.style.width = "350px";
             startProjection(nameIn.value, invest, add, rate, years);
          catch(e)
             set the body width to 700px (like code above)
-            errBox.innerText = e.message;
+            errBox.textContent = e.message;
      */
+    try {
+        if (!isValid) {
+            throw new Error("Please correct the entries highlighted below.");
+        }
+
+        document.body.style.width = "350px";
+        startProjection(nameIn.value, invest, add, rate, years);
+    } catch (e) {
+        document.body.style.width = "700px";
+        errBox.textContent = e.message;
+    }
 };
 
 const startProjection = (name, bal, add, rate, years) => {
@@ -84,39 +118,72 @@ const startProjection = (name, bal, add, rate, years) => {
     statusMsg.style.color = "red";
     let count = 1;
 
-    // TO-DO: startYear = the current year
     const startYear = new Date().getFullYear();
 
     let formattedBal = formatter.format(bal);
-    output.innerHTML = `Year ${startYear} = ${formattedBal}`;
+    output.textContent = `Year ${startYear} = ${formattedBal}`;
 
-    /* TODO: setup an interval to do the following
+    projectionTimer = setInterval(() => {
+    /* TODO: code the interval logic
         for (let i = 0; i < 12; i++) {
             bal = ((bal + add) * (1 + (rate / 12 / 100))).toFixed(2);
         }
-        format the balance like the starting code above
-        update the output like the starting code above
+        format the balance - see code above
+        update the output - see code above
         if count is >= years
-            clear interval
-            update the statusMsg like the starting code above
-            set the statusMsg color to green like the starting code above
+            clear interval projectionTimer (Ch 8)
+            update the statusMsg to Calculation Completed! (like code above)
+            set the statusMsg to red (like code above)
         end if
         add one to the count
      */
+        for (let i = 0; i < 12; i++) {
+            bal = ((bal + add) * (1 + (rate / 12 / 100))).toFixed(2);
+        }
+        const formatted = formatter.format(bal);
+        output.textContent = `Year ${startYear} = ${formatted}`;
+
+        if (count => years){
+            clearInterval(projectionTimer);
+            statusMsg.textContent = "Calculation Complete!";
+            statusMsg.style.color = "red";
+        }
+
+
+
+    }, 1000);
 };
 
 const setTestData = () => {
     resetForm();
-    // TODO: set default values for all input fields
+    /* TODO: set default value for all input fields
+        Setup the future date to 10 years from now:
+        (1) create a const variable named future and set it to the current date (Ch 8)
+        (2) add 10 years to the future date variable (Ch 8)
+        (3) use toISOString().split('T')[0] to display the future date (Ch 8)
+     */
+    nameIn.value = "Joe Smith";
+    emailIn.value = "josmith@wsc.edu";
+    investIn.value = "10000";
+    addIn.value = "500";
+    rateIn.value = "5.5";
+
+    const future = new Date();
+    future.setFullYear(future.getFullYear())
 };
 
 const resetForm = () => {
     /* TODO:
-        clear all input fields (done via html btw)
-        clear the interval
-        document.querySelectorAll(".error").forEach(s => s.textContent = "*");
-        set the body width to 350px (like code above)
-        set the focus to the name input field
+        Using textContent clear the following error spans
+            errBox (#error_message)
+            output (#projection_output)
+            statusMsg (#status_message)
+        clear the interval projectionTimer (Ch 8)
+        reset all the error spans back to *
+            document.querySelectorAll(".error").forEach(s => s.textContent = "*");
+        set the body width to 350px (see code example above)
+        set the statusMsg to red (see code example above)
+        set the focus to the name input field (Ch 9)
      */
 };
 
